ce918930d38b54a645bf558891aaba4bb25b79b0
candec <omnius.candec@gmail.com>
2020-11-27 21:27:29 +0000

    start libft.h

---
+++ lvl_0/ft_memmove.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jibanez- <jibanez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/26 17:46:57 by jibanez-          #+#    #+#             */
+/*   Updated: 2020/11/26 17:46:57 by jibanez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char *s;
+	unsigned char *d;
+
+	s = (unsigned char *)src;
+	d = (unsigned char *)dest;
+	if (d == s)
+		return (dest);
+	else if (d > s)
+	{
+		s = s + n - 1;
+		d = d + n - 1;
+		while (n--)
+			*d-- = *s--;
+	}
+	else
+		ft_memmove(d, s, n);
+	return (dest);
+}
