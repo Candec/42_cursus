b7474cffb453b0a63801815e264728a56f361190
candec <omnius.candec@gmail.com>
2020-12-01 16:56:49 +0000

    a

---
+++ lvl_0/libft/ft_memchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jibanez- <jibanez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/26 19:04:19 by jibanez-          #+#    #+#             */
+/*   Updated: 2020/11/26 19:04:19 by jibanez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*ch;
+
+	i = 0;
+	ch = (unsigned char*)s;
+	while (i < n)
+	{
+		if (s[i] == (unsigned char)c)
+			return (ch[i]);
+		i++;
+	}
+	return (0);
+}
---
+++ lvl_0/libft/ft_memcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jibanez- <jibanez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/26 16:44:28 by jibanez-          #+#    #+#             */
+/*   Updated: 2020/11/26 16:44:28 by jibanez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char *s;
+	unsigned char *d;
+
+	s = (unsigned char *)src;
+	d = (unsigned char *)dest;
+	if (dest == 0 && src == 0)
+		return (0);
+	if (dest == src)
+		return (dest);
+	while (n--)
+		*d++ = *s++;
+	return (dest);
+}
