4d5a6cd2fc8724c9901c107a73dadad5c6d3d56e
Jesus Ibanez lopez <jibanez-@c1r10s3.42lisboa.com>
2021-02-24 18:29:53 +0000

    stardup duplication and old version of itoa check

--- lvl_0/libft/ft_strdup.c
+++ lvl_0/libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: jibanez- <jibanez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 17:14:30 by jibanez-          #+#    #+#             */
-/*   Updated: 2021/02/24 12:25:21 by jibanez-         ###   ########.fr       */
+/*   Updated: 2021/02/24 18:20:39 by jibanez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,13 @@
 
 char	*ft_strdup(const char *src)
 {
-	int		i;
+	int		size;
 	char	*dest;
 
-	i = 0;
-	dest = malloc(ft_strlen(src) * sizeof(*dest));
+	size = ft_strlen(src);
+	dest = malloc(size * sizeof(char) + 1);
 	if (dest == '\0')
 		return (0);
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
+	ft_strlcpy(dest, src, size + 1);
 	return (dest);
 }
