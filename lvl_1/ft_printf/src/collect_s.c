9dbcd8f6af93259c1270fa0dcfa32ae91916a459
candec <omnius.candec@gmail.com>
2021-04-26 19:22:03 +0100

    collect s finished??

--- lvl_1/ft_printf/src/collect_s.c
+++ lvl_1/ft_printf/src/collect_s.c
@@ -1,57 +1,61 @@
 #include "ft_printf.h"
 
-static	void	s_precision(t_printf *p, char *str, int len)
+static	int	s_size(t_printf *p, char *str)
 {
-	int i;
-	char c;
+	if (ft_strlen(str) < p->var.precision)
+		return (ft_strlen(str));
+	return (p->var.precision);
+}
+
+static	void	s_precision(t_printf *p, char *str, int pre)
+{
+	int		i;
+	char	c;
 
 	i = 0;
-
-	if (p->var.precision < len && p->var.precision > 0)
+	if (p->var.precision >= 6 && str == "(null)")
+		p->ret += write(1, "(null)", 6);
+	else if (p->var.precision == -1 && str == "(null)")
+		p->ret += write(1, "(null)", 6);
+	else if (p->var.precision < 6 && str == "(null)")
+		return ;
+	else if (p->var.precision >= 0)
 	{
-		while (i < p->var.precision)
+		while (i < pre)
 		{
 			c = str[i];
 			p->ret += write(1, &c, 1);
 			i++;
 		}
 	}
-	else if (p->var.precision == 0)
-		p->ret += write(1, NULL, 1);
 	else
-		p->ret += write(1, str, len);
+		p->ret += write(1, str, ft_strlen(str));
 }
 
 void	collect_s(t_printf *p)
 {
 	char	*str;
 	int		i;
-	int		j;
-	int		len;
+	int		pre;
 
 	str = va_arg(p->args, char *);
-	len = ft_strlen(str);
-	j = len;
+	if (!str)
+		str = "(null)";
+	pre = s_size(p, str);
 	i = 0;
-
-
-	if (p->var.precision > 0)
-	 	j = len - p->var.precision;
-	if (p->var.width > len)
-		i = p->var.width - p->var.precision;
-
-
+	if (p->var.width > pre)
+		i = p->var.width - pre;
 	if (p->flags.minus == false)
 	{
 		if (p->flags.zero == true)
 			print_loop(p, '0', i);
 		else if (p->flags.zero == false)
 			print_loop(p, ' ', i);
-		s_precision(p, str, j);
+		s_precision(p, str, pre);
 	}
 	else if (p->flags.minus == true)
 	{
-		s_precision(p, str, j);
+		s_precision(p, str, pre);
 		print_loop(p, ' ', i);
 	}
 }
--- lvl_1/ft_printf/src/main.c
+++ lvl_1/ft_printf/src/main.c
@@ -7,9 +7,9 @@ int	main(void)
 	printf("++++++++++++++++\n");
 	printf("\n");
 	printf("Expected:\n");
-	printf("check check check me\n");
+	printf("[%d]\n",printf("check check check me\n"));
 	printf("Result:\n");
-	ft_printf("check check check me\n");
+	printf("[%d]\n",ft_printf("check check check me\n"));
 	printf("\n");
 	printf("\n");
 	printf("\n");
@@ -19,9 +19,9 @@ int	main(void)
 	printf("++++++++++++++++\n");
 	printf("\n");
 	printf("Expected:\n");
-	printf("%s\n", "This is a test");
+	printf("[%d]\n",printf("%s\n", "This is a test"));
 	printf("Result:\n");
-	ft_printf("%s\n", "This is a test");
+	printf("[%d]\n",ft_printf("%s\n", "This is a test"));
 	printf("\n");
 	printf("\n");
 	printf("\n");
@@ -31,9 +31,9 @@ int	main(void)
 	printf("++++++++++++++++\n");
 	printf("\n");
 	printf("Expected:\n");
-	printf("%s%s%s%s\n", "123", "456", "789", "This Was a Test");
+	printf("[%d]\n",printf("%s%s%s%s\n", "123", "456", "789", "This Was a Test"));
 	printf("Result:\n");
-	ft_printf("%s%s%s%s\n", "123", "456", "789", "This Was a Test");
+	printf("[%d]\n",ft_printf("%s%s%s%s\n", "123", "456", "789", "This Was a Test"));
 	printf("\n");
 	printf("\n");
 	printf("\n");
@@ -43,9 +43,9 @@ int	main(void)
 	printf("++++++++++++++++\n");
 	printf("\n");
 	printf("Expected:\n");
-	printf("%s%s%s %s, and it was %s test\n", "123", "456", "789", "This Was a", "good");
+	printf("[%d]\n",printf("%s%s%s %s, and it was %s test\n", "123", "456", "789", "This Was a", "good"));
 	printf("Result:\n");
-	ft_printf("%s%s%s %s, and it was %s test\n", "123", "456", "789", "This Was a", "good");
+	printf("[%d]\n",ft_printf("%s%s%s %s, and it was %s test\n", "123", "456", "789", "This Was a", "good"));
 	printf("\n");
 	printf("\n");
 	printf("\n");
@@ -55,9 +55,9 @@ int	main(void)
 	printf("++++++++++++++++\n");
 	printf("\n");
 	printf("Expected:\n");
-	printf("%s%s%s %s, and it was %c%c%c%c test\n", "123", "456", "789", "numbers", 'g', 'o', 'o', 'd');
+	printf("[%d]\n",printf("%s%s%s %s, and it was %c%c%c%c test\n", "123", "456", "789", "numbers", 'g', 'o', 'o', 'd'));
 	printf("Result:\n");
-	ft_printf("%s%s%s %s, and it was %c%c%c%c test\n", "123", "456", "789", "numbers", 'g', 'o', 'o', 'd');
+	printf("[%d]\n",ft_printf("%s%s%s %s, and it was %c%c%c%c test\n", "123", "456", "789", "numbers", 'g', 'o', 'o', 'd'));
 	printf("\n");
 	printf("\n");
 	printf("\n");
@@ -67,9 +67,9 @@ int	main(void)
 	printf("++++++++++++++++\n");
 	printf("\n");
 	printf("Expected:\n");
-	printf("Madonna %20.5s Madonna\n", "Incredible width");
+	printf("[%d]\n",printf("Madonna %20.5s Madonna\n", "Incredible width"));
 	printf("Result:\n");
-	ft_printf("Madonna %20.5s Madonna\n", "Incredible width");
+	printf("[%d]\n",ft_printf("Madonna %20.5s Madonna\n", "Incredible width"));
 	printf("\n");
 	printf("\n");
 	printf("\n");
@@ -79,9 +79,114 @@ int	main(void)
 	printf("++++++++++++++++\n");
 	printf("\n");
 	printf("Expected:\n");
-	printf("Madonna %-7.5s Madonna\n", "Incredible width");
+	printf("[%d]\n",printf("a%-7.5sa\n", "Incredible width"));
 	printf("Result:\n");
-	ft_printf("Madonna %-7.5s Madonna\n", "Incredible width");
+	printf("[%d]\n",ft_printf("a%-7.5sa\n", "Incredible width"));
+	printf("\n");
+	printf("\n");
+	printf("\n");
+
+
+	printf("test 8 - String Width greater than Var1 & Precision\n");
+	printf("++++++++++++++++\n");
+	printf("\n");
+	printf("Expected:\n");
+	printf("[%d]\n", printf("x%.5sx\n", "Wellcome"));
+	printf("Result:\n");
+	printf("[%d]\n", ft_printf("x%.5sx\n", "Wellcome"));
+	printf("\n");
+	printf("\n");
+	printf("\n");
+
+
+	printf("test 8 - String Width greater than Var1 & Precision\n");
+	printf("++++++++++++++++\n");
+	printf("\n");
+	printf("Expected:\n");
+	printf("[%d]\n", printf("x%20.5sx\n", "Wellcome"));
+	printf("Result:\n");
+	printf("[%d]\n", ft_printf("x%20.5sx\n", "Wellcome"));
+	printf("\n");
+	printf("\n");
+	printf("\n");
+
+
+	printf("test 9 - String NULL\n");
+	printf("++++++++++++++++\n");
+	printf("\n");
+	printf("Expected:\n");
+	printf("[%d]\n", printf("test %s\n", NULL));
+	printf("Result:\n");
+	printf("[%d]\n", ft_printf("test %s\n", NULL));
+	printf("\n");
+	printf("\n");
+	printf("\n");
+
+
+	printf("test 10 - String exact precision NULL\n");
+	printf("++++++++++++++++\n");
+	printf("\n");
+	printf("Expected:\n");
+	printf("[%d]\n", printf("test %.7s\n", NULL));
+	printf("Result:\n");
+	printf("[%d]\n", ft_printf("test %.7s\n", NULL));
+	printf("\n");
+	printf("\n");
+	printf("\n");
+
+	printf("test 11 - String over precision NULL\n");
+	printf("++++++++++++++++\n");
+	printf("\n");
+	printf("Expected:\n");
+	printf("[%d]\n", printf("test %.10s\n", NULL));
+	printf("Result:\n");
+	printf("[%d]\n", ft_printf("test %.10s\n", NULL));
+	printf("\n");
+	printf("\n");
+	printf("\n");
+
+
+	printf("test 12 - String under precision NULL\n");
+	printf("++++++++++++++++\n");
+	printf("\n");
+	printf("Expected:\n");
+	printf("[%d]\n", printf("%.5s\n", NULL));
+	printf("Result:\n");
+	printf("[%d]\n", ft_printf("%.5s\n", NULL));
+	printf("\n");
+	printf("\n");
+	printf("\n");
+
+
+	printf("test 12 - String precision 0 NULL\n");
+	printf("++++++++++++++++\n");
+	printf("\n");
+	printf("Expected:\n");
+	printf("[%d]\n", printf("%.0s\n", "test"));
+	printf("Result:\n");
+	printf("[%d]\n", ft_printf("%.0s\n", "test"));
+	printf("\n");
+	printf("\n");
+	printf("\n");
+
+	printf("test 13 - String precision 0 NULL with width\n");
+	printf("++++++++++++++++\n");
+	printf("\n");
+	printf("Expected:\n");
+	printf("[%d]\n", printf("%10.0s\n", "test"));
+	printf("Result:\n");
+	printf("[%d]\n", ft_printf("%10.0s\n", "test"));
+	printf("\n");
+	printf("\n");
+	printf("\n");
+
+	printf("test 14 - String precision 0 NULL with negative width\n");
+	printf("++++++++++++++++\n");
+	printf("\n");
+	printf("Expected:\n");
+	printf("[%d]\n", printf("%-10.0s\n", "test"));
+	printf("Result:\n");
+	printf("[%d]\n", ft_printf("%-10.0s\n", "test"));
 	printf("\n");
 	printf("\n");
 	printf("\n");
