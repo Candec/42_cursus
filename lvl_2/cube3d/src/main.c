007fc4b359a3ee4b259920603ea79742313968b5
candec <omnius.candec@gmail.com>
2021-05-11 15:46:02 +0100

    main.c window drawing test

--- lvl_2/cube3d/src/main.c
+++ lvl_2/cube3d/src/main.c
@@ -1,11 +1,11 @@
-#include <mlx.h>
+#include "mlx.h"
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
+	void	*mlx_ptr;
+	void	*win_ptr;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	mlx_loop(mlx);
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "a");
+	mlx_loop(mlx_ptr);
 }
