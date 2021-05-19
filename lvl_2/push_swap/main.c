da37538f1946cb20b6b6902a388fd882be428781
candec <omnius.candec@gmail.com>
2021-05-19 21:37:33 +0100

    now we can see the stack

--- lvl_2/push_swap/main.c
+++ lvl_2/push_swap/main.c
@@ -3,11 +3,14 @@
 int	main(int argc, char *argv[])
 {
 	t_list *a;
-	t_list *b;
 
-	a = 0;
-	b = 0;
+	a = NULL;
 	if (argc == 1)
 		return (1);
 	ft_stack_init(argv + 1, argc - 1, &a);
+	while (a != NULL)
+	{
+		printf("%lld\n", (long long)a->content);
+		a = a->next;
+	}
 }
