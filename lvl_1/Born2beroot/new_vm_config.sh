b84a5e8966c22f45754847915637ec5acdf3d3a2
Jesus Ibanez lopez <jibanez-@c1r18s10.42lisboa.com>
2021-05-28 18:01:20 +0100

    VBox creator and configurator

---
+++ lvl_1/Born2beroot/new_vm_config.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+VB_Name='Devian_Test'
+VB_OS='Debian_64'
+VB_StoragePath="/Users/$USER/VB_Name.vdi"
+VB_ISOPath="/Users/$USER/Downloads/debian-10.9.0-amd64-netinst.iso"
+
+# Creates the VirtualBox and expects an OS. Different OS can be selected using the command 
+# 	$ VBoxManage ostype
+VBoxManage createvm --name $VB_Name --ostype $VB_OS --register
+
+# Sets the main characteristics of the VB [CPUS, Memory, VRAM and Storage capacity]
+# It can never be greater than the computer it is hosting the VB
+VBoxManage modifyvm $VB_Name --cpus 1 --memory 2048 --vram 16
+VBoxManage createhd --filename $VB_StoragePath --size 5120
+
+# Sets up a Dynamic Space Unit, rather than a fixed one
+--variant Standard
+
+# Makes the VB boot from the SATA
+VBoxManage storagectl $VB_Name --name "SATA Controller" --add sata --bootable on
+
+# Gives control of the SATA to the VB
+VBoxManage storageattach $VB_Name --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $VB_StoragePath
+
+# Adds a virtual CD/DVD drive so it can read the ISO with the OS
+VBoxManage storagectl $VB_Name --name "IDE Controller" --add ide
+
+# Mounts the ISO into the virtual CD/DVD drive
+VBoxManage storageattach $VB_Name --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium $VB_ISOPath
+
+# Displays the info of the VB
+VBoxManage showvminfo $VB_Name
