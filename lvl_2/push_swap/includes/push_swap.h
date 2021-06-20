7c4393f5678129d8527b59361cb7441154721afa
candec <omnius.candec@gmail.com>
2021-06-20 19:12:58 +0100

    visualizer added

--- lvl_2/push_swap/includes/push_swap.h
+++ lvl_2/push_swap/includes/push_swap.h
@@ -10,5 +10,6 @@
 */
 int		push_swap(int a[]);
 void	ft_stack_init(char *stack[], int i, t_list **a);
+void	visualize_stack(t_list *a, t_list *b);
 
 #endif
--- lvl_2/push_swap/main.c
+++ lvl_2/push_swap/main.c
@@ -3,14 +3,33 @@
 int	main(int argc, char *argv[])
 {
 	t_list *a;
+	t_list *b;
 
 	a = NULL;
+	b = NULL;
 	if (argc == 1)
 		return (1);
 	ft_stack_init(argv + 1, argc - 1, &a);
-	while (a != NULL)
-	{
-		printf("%lld\n", (long long)a->content);
-		a = a->next;
-	}
+	visualize_stack(a, b);
+	// while (a != NULL || b !=NULL)
+	// {
+	// 	if (a != NULL)
+	// 	{
+	// 		printf("%lld	", (long long)a->content);
+	// 		a = a->next;
+	// 	}
+	// 	else
+	// 	{
+	// 		printf(" 	");
+	// 	}
+	// 	if (b != NULL)
+	// 	{
+	// 		printf("%lld\n", (long long)b->content);
+	// 		b = b ->next;
+	// 	}
+	// 	else
+	// 	{
+	// 		printf(" \n");
+	// 	}
+	// }
 }
