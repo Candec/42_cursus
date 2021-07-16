f9b386f1684e21403c537e4be4115b2906e71e1d
candec <omnius.candec@gmail.com>
2021-07-16 20:12:16 +0100

    compare function to simplificate

--- lvl_2/push_swap/src/parse.c
+++ lvl_2/push_swap/src/parse.c
@@ -1,35 +1,35 @@
 #include "../includes/push_swap.h"
 
-int parse_int(int argc, char *argv[])
+static int	compare(char **strs, int i)
 {
-	int i;
-	char **store;
+	while (strs[i] != NULL)
+	{
+		if (!ft_isnumber(strs[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	parse_int(int argc, char *argv[])
+{
+	int		i;
+	int		rtr;
+	char	**store;
 
 	if (argc <= 1)
 		return (0);
 	else if (argc == 2)
+	{
 		if (ft_strchr(argv[1], ' '))
-		{
-			i = 0;
-			store = ft_split(argv[1], ' ');
-			while (store[i] != NULL)
-			{
-				if (!ft_isnumber(store[i]))
-					return (0);
-				i++;
-			}
-		}
+			rtr = compare(ft_split(argv[1], ' '), 0);
 		else if (!ft_isnumber(argv[1]))
 			return (0);
-	else
-	{
-		i = 1;
-		while (argv[i] != NULL)
-		{
-			if (!ft_isnumber(argv[i]))
-				return (0);
-			i++;
-		}
 	}
-return 1;
+	else
+		rtr = compare(argv, 1);
+
+		// Look for repeated arguments
+
+	return (rtr);
 }
