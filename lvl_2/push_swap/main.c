985629ad0cf88b3bffc31a6b2382892125c047f0
candec <omnius.candec@gmail.com>
2021-07-20 13:29:05 +0100

    making instructions

--- lvl_2/push_swap/main.c
+++ lvl_2/push_swap/main.c
@@ -1,5 +1,34 @@
 #include "./includes/push_swap.h"
 
+static int		issorted(t_node *node, int until, int anode)
+{
+	return (ksorted(node, anode) >= until);
+}
+
+static void		makeinstruct(t_node **nodea)
+{
+	t_stacks	stacks;
+	t_node		*nodeb;
+	t_todo		**list;
+	int			size;
+
+	nodeb = NULL;
+	stacks.a = nodea;
+	stacks.b = &nodeb;
+	size = countitem(*(stacks.a));
+	if (!issorted(*(stacks.a), size, 1))
+	{
+		if (size <= 3)
+			smallsort(stacks);
+		else
+			quicksort(stacks, size, 1, 2);
+		list = getlist();
+		while (del(list))
+			;
+		printinstruct();
+	}
+}
+
 int	main(int argc, char *argv[])
 {
 	t_node	*node;
@@ -9,6 +38,9 @@ int	main(int argc, char *argv[])
 	node = NULL;
 	str = ft_strmerge(argv + 1, 1, argc - 1);
 	if (!(parse(str, &node, &verbose) < 1 || verbose))
-		return (0);
-	return (1);
+			makeinstruct(&node);
+	else if (argc > 1)
+		ft_putstr("Error\n");
+	ft_strdel(&str);
+	return (0);
 }
