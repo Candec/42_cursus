da34e0c205f3d32feb5449b0440f1f3b03185b30
candec <omnius.candec@gmail.com>
2021-08-27 13:29:50 +0100

    exam rank 2 updated

--- Exams/Rank_2/Get_next_line/get_next_line.c
+++ Exams/Rank_2/Get_next_line/get_next_line.c
@@ -3,22 +3,22 @@
 
 int get_next_line(char** line)
 {
-    char buff;
-    int i;
+	char buff;
+	int i;
 
-    i = 0;
-    *line = malloc(8000);
-    if (!*line)
-        return (-1);
-    while (read(0, &buff, 1))
-    {
-        if (buff == '\n')
-        {
-            (*line)[i] = '\0';
-            return (1);
-        }
-        (*line)[i++] = buff;
-    }
-    (*line)[i] = '\0';
-    return (0);
+	i = 0;
+	*line = malloc(8000);
+	if (!*line)
+		return (-1);
+	while (read(0, &buff, 1))
+	{
+		if (buff == '\n')
+		{
+			(*line)[i] = '\0';
+			return (1);
+		}
+		(*line)[i++] = buff;
+	}
+	(*line)[i] = '\0';
+	return (0);
 }
