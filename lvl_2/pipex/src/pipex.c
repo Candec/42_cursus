d8cef1ea511b0b1ee9a59a77a5c4a3dd4658946e
candec <omnius.candec@gmail.com>
2021-08-28 10:18:27 +0100

    pipex genesis

--- lvl_2/pipex/src/pipex.c
+++ lvl_2/pipex/src/pipex.c
@@ -6,7 +6,7 @@
 **	2. No input file or no access to the input file
 **	3. Problems opening the input file
 **	4. Problems opening the output file
-**	5. Commands not found or incorrect
+**	5. Command(s) not found or incorrect
 **
 **	Extra check to clear if the code could not continue
 */
@@ -37,16 +37,45 @@ void	error_handling(t_pipex *p, char *error, int exit)
 	}
 }
 
+/*
+**	1. As Here_doc has 1 more argument, it needs to be accounted for
+**	2. Looks for the commands in the enviroments var using the paths
+**	3. Creates or opens the file "inputstream.txt"
+**	4. Handles errors with the file
+**	5. Opens the file indicated in the arguments
+**	6. Handles errors with the file
+*/
+
+void	check_heredoc_mode(t_pipex *p, int argc, char *argv[], char *envp[])
+{
+	if (argc < 6)
+		error_handling(p, "HERE_DOC MODE USAGE", TRUE);
+	find_command_paths(p, argc, argv, envp);
+	p->fd_input = open("inputstream.txt", O_WRONLY | O_CREAT, 0777);
+	if (p->fd_input == ERROR)
+		error_handling(p, "OPENING INPUTSTREAM", TRUE);
+	p->fd_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_APPEND, 0777);
+	if (p->fd_out == ERROR)
+		error_handling(p, "OPENING OUTPUT FILE", TRUE);
+	p->limiter = ft_strdup(argv[2]);
+}
+
+void	check_pipe_mode(t_pipex *p, int argc, char *argv[], char *envp[])
+{
+	if (access())
+}
+
 void	check_input(t_pipex *p, int argc, char *argv[], char *envp[])
 {
 	if (argc < 5)
 		error_handling(p, "INSUFICIENT ARGUMENTS", TRUE);
 	if (p->mode == HERE_DOC)
-	{
-		if (argc < 6)
-			error_handling(p, "INSUFICIENT ARGUMENTS", TRUE);
-
-	}
+		check_heredoc_mode(p, argc, argv, envp);
+	else
+		check_pipe_mode(p, argc, argv, envp);
+	p->argc = argc;
+	p->argv = argv;
+	p->envp = envp;
 }
 
 void	pipex_init(t_pipex *p, char **argv)
